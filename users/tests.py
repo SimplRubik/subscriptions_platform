e74372c2df1093cbd5c31ce6c043049172f4e58f
Ruben <8649499687@my.lerna.am>
2025-08-19 16:05:45 +0400

    Add CustomUser model, JWT auth, profile CRUD and tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import CustomUser
 
-# Create your tests here.
+class UserAuthTests(APITestCase):
+
+    def test_register_user(self):
+        url = reverse('register')
+        data = {
+            'username': 'usertest',
+            'email': 'test@gmail.com',
+            'password': 'Pass12371!',
+            'password2': 'Pass12371!',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CustomUser.objects.count(), 1)
+        self.assertEqual(CustomUser.objects.get().username, 'usertest')
+    
+    def test_login_user(self):
+        user = CustomUser.objects.create_user(username='usertest', email='test@gmail.com', password='Pass12371!')
+        url = reverse('token_obtain_pair')
+        data = {
+            'username': 'usertest',
+            'password': 'Pass12371!'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_profile_access(self):
+        user = CustomUser.objects.create_user(username='usertest', password='Pass12371!')
+        url = reverse('profile')
+        self.client.force_authenticate(user=user)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['username'], 'usertest')
+
+        
+
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .serializers import RegisterSerializer, ProfileSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
+        return self.request.user
